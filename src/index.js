9bc5dcbc915ab59858a571225e278d26c560f98c
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2024-11-23 15:10:41 -0300

    Improve reusability of StarRating component by using props to format several things on it, and add PropTypes in order to ensure the right type of data that the component expects

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
   </React.StrictMode>
 );
