16a115173525f1c0669152e55a3fb9e104072023
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2024-12-15 18:03:38 -0300

    Utilize useEffect hook for the first time to get movies from omdb api using fetch and promises, and change initial state for movies and watched

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -49,9 +49,17 @@ const tempWatchedData = [
 
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const OMDB_API_KEY = "6fc5422e";
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
 
   return (
     <>
